23a16b801b16f5d6382f60c656da30f49bac8b6b
Yusuke Suzuki <4348519756@g.ecc.u-tokyo.ac.jp>
2022-01-18 14:14:00 +0900

    rename

--- traverse/src/traverse.c
+++ traverse/src/traverse.c
@@ -3,10 +3,10 @@
 
 void traverse(const Node *n)
 {
-    if (n == NULL) return;
-    
-    // 以下に木を操作する手順を実装する
-    // どうすればよいか？
-    // 表示は printf("value = %d\n", n->value); のような形で良い
-    
+    if (n == NULL)
+        return;
+
+    printf("value=%d\n", n->value);
+    traverse(n->left);
+    traverse(n->right);
 }
